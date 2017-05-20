c104346be4dc38f30441d7a7ffa539c6a53491be
anubhamandal <anubha.mandal@sjsu.edu>
2017-05-20 08:06:16 -0700

    application changes

--- src/main/java/com/springJava/jobTracker/repo/JobRepo.java
+++ src/main/java/com/springJava/jobTracker/repo/JobRepo.java
@@ -25,7 +25,12 @@ public interface JobRepo extends CrudRepository<Job, Long>{
 		
 	@Transactional
     @Modifying(clearAutomatically = true)
-	@Query("update Job p set p.jobtitle=?1, p.skill=?2, p.description=?3, p.location=?4, p.salary=?5, p.status=?6 where p.id=?7")
-	void updateJobDetails(String job_title, String skill, String desc, String location, int salary, JobStatus status, Long id);
+	@Query("update Job p set p.jobtitle=?1, p.skill=?2, p.description=?3, p.location=?4, p.salary=?5 where p.id=?6")
+	void updateJobDetails(String job_title, String skill, String desc, String location, int salary, Long id);
+	
+	@Transactional
+    @Modifying(clearAutomatically = true)
+	@Query("update Job p set p.status=?1 where p.id=?6")
+	void updateJobStatus(JobStatus status, Long id);
 
 }
